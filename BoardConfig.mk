fef46f006e0ad4aacef0365abfd435a9b4ab1769
lawn <bloom.all.over.12@gmail.com>
2016-11-24 05:52:17 +0900

    Move cmdline to hlte-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,6 @@ TARGET_OTA_ASSERT_DEVICE := SCL22,hltekdi
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_kdi_defconfig
 
-#workaround for Android L (selinux permissive)
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.selinux=permissive
-
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8974
 TARGET_LIBINIT_MSM8974_DEFINES_FILE := device/samsung/hltekdi/init/init_hlte.cpp
